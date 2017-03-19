8dc11fb86670998bdfa50a160ab82e84c68d6b28
hyuruk <hyuruk@gmail.com>
2017-03-19 22:22:43 +0300

    report is prepared by using matlab report generator

---
+++ input_paramaters.m
@@ -0,0 +1,7 @@
+%% input parameters
+deltaposdeg = 90; %% [degree]  
+period = 0.2; %% [seconds] T   45° --> -45° 0.1s
+inertiaazimuth = 1.6; %% [kg.m^2] motor inertia is not included
+inertiaelevation = 0.5; %% [kg.m^2] motor inertia is not included
+azimuthloadmass = 50; %% [kg]
+elevationloadmass = 25; %% [kg]
---
+++ velaccpowertorquecalc.m
@@ -0,0 +1,65 @@
+%% velocity, acc, power, torque calculations
+%% assume sinusoidal profiles
+%% theta = A*cos(wt)   --> position
+%% angvel = -A*w*sin(wt) --> angular velocity dtheta/dt
+%% angacc = -A*w^2*cos(wt) --> angular acceleration dangvel/dt
+%% desired requirement : 90° --> 100ms
+deltaposdeg = 90; %% [degree] 
+deltaposrad = deltaposdeg*2*pi / 360; %% [rad]  2*A
+period = 0.2; %% [seconds] T
+angfreg = 2*pi/period; %% [rad/s]  w = 2pi/T
+maxangvelrads = (deltaposrad/2) * angfreg; %% [rad/s]
+maxangveldegs = maxangvelrads * 360 / (2*pi); %% [deg/s]
+maxangvelrpm = maxangvelrads * 60 / (2*pi); %% [rpm]
+maxangaccrads2 = (deltaposrad/2) * angfreg^2; %% [rad/s^2]
+maxangaccdegs2 = maxangaccrads2 * 360 / (2*pi); %% [deg/s^2]
+%% torque = inertia * acc
+%% power = torque * angvel
+inertiaazimuth = 1.6; %% [kg.m^2] 
+maxtorqueazimuth = inertiaazimuth * maxangaccrads2; %% [Nm]
+%% plot the assumed profiles
+tres = period / 100; %% [seconds]
+t = 0:tres:period;
+posdegarr = deltaposdeg/2 * cos(angfreg*t);
+figure;
+plot(t,posdegarr);
+grid;
+title('position of the load vs time')
+xlabel('time [seconds]');
+ylabel('position [degree]');
+saveas(gcf, 'position_load', 'jpg')
+angveldegsarr = -1*maxangveldegs * sin(angfreg*t);
+figure;
+plot(t,angveldegsarr);
+grid;
+title('angular velocity of the load vs time')
+xlabel('time [seconds]');
+ylabel('angular velocity [degree/s]');
+saveas(gcf, 'velocity_load', 'jpg')
+angaccdegs2arr = -1*maxangaccdegs2 * cos(angfreg*t);
+figure;
+plot(t,angaccdegs2arr);
+grid;
+title('angular acceleration of the load vs time')
+xlabel('time [seconds]');
+ylabel('angular acceleration [degree/s^2]');
+saveas(gcf, 'acceleration_load', 'jpg')
+angaccrads2arr = -1*maxangaccrads2 * cos(angfreg*t);
+torqueazimutharr = inertiaazimuth * angaccrads2arr;
+figure;
+plot(t,torqueazimutharr);
+grid;
+title('requirred torque of the load vs time')
+xlabel('time [seconds]');
+ylabel('torque [Nm]');
+saveas(gcf, 'torque_load', 'jpg')
+angvelradsarr = -1*maxangvelrads * sin(angfreg*t);
+powerazimutharr = torqueazimutharr .* angvelradsarr;
+maxpowerazimuth = max(powerazimutharr); %% [watt]
+figure;
+plot(t,powerazimutharr);
+grid;
+title('requirred power of the load vs time')
+xlabel('time [seconds]');
+ylabel('power [Watt]');
+saveas(gcf, 'power_load', 'jpg')
